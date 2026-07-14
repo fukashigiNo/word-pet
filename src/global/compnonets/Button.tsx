945df59bde8729f7fc6c708cf32c623aa6019a8d
fukashigiNo <zemvikak@gmail.com>
2026-07-14 17:51:50 +0500

    fixed bugs

--- src/global/compnonets/Button.tsx
+++ src/global/compnonets/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 interface IButton {
-    text: string,
+    text: any,
     onClick: () => void
     className?: string
 }
@@ -9,7 +9,7 @@ interface IButton {
 export default function Button({
     text, 
     onClick, 
-    className = "flex justify-center items-center border w-[32px] rounded-md h-[32px] bg-slate-50 text-semibold focus:border focus:border-blue-400 focus:text-[20px]"
+    className,
 }: IButton) {
   return (
     <button onClick={onClick} className={className}>
